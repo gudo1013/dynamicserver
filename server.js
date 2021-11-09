84e27ec3551298d01804c7a93b9b4e168b88caf0
njpon <pont7171@stthomas.edu>
2021-11-08 20:56:17 -0600

    added year.html template functionality and included charting module

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ let path = require('path');
 // NPM modules
 let express = require('express');
 let sqlite3 = require('sqlite3');
+let chart = require('chart.js');
 
 
 let public_dir = path.join(__dirname, 'public');
@@ -13,6 +14,9 @@ let db_filename = path.join(__dirname, 'db', 'usenergy.sqlite3');
 
 let app = express();
 let port = 8000;
+
+
+
 
 // Open usenergy.sqlite3 database
 let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
@@ -36,25 +40,27 @@ app.get('/', (req, res) => {
 // GET request handler for '/year/*'
 app.get('/year/:selected_year', (req, res) => {
     console.log(req.params.selected_year);
-    fs.readFile(path.join(template_dir, 'year.html'), (err, template) => {
+    fs.readFile(path.join(template_dir, 'year.html'), 'utf-8', (err, template) => {
         // modify `template` and send response
         // this will require a query to the SQL database
         if (err) {
             res.status(404).send('404 Error not found');
         } else {
-            let year_response = data.replace('{{{YEAR}}}', req.params.selected_year);
+            let response = template.replace('{{{YEAR}}}', req.params.selected_year);
 
             db.all('SELECT * from Consumption WHERE year = ?', [req.params.selected_year], (err, rows) => {
-                let coal_response = data.replace('{{{COAL_COUNT}}}', rows[0].coal); // rows[0] has all elements, rows[0].coal is the coal value
-                let natural_gas_reponse = data.replace('{{{NATURAL_GAS_COUNT}}}', rows[0].natural_gas);
-                let nuclear_response = data.replace('{{{NUCLEAR_COUNT}}}', rows[0].nuclear);
-                let petroleum_reponse = data.replace('{{{PETROLEUM_COUNT}}}', rows[0].petroleum);
-                let renewable_reponse = data.replace('{{{RENEWABLE_COUNT}}}', rows[0].renewable);
-                res.status(200).type('html').send(response); // is this correct thing / time to send right here
+
+                if (err) {
+                    res.status(404).send("Could not finish query");
+                } else {
+                response = response.replace('{{{COAL_COUNT}}}', rows[0].coal); 
+                response = response.replace('{{{NATURAL_GAS_COUNT}}}', rows[0].natural_gas);
+                response = response.replace('{{{NUCLEAR_COUNT}}}', rows[0].nuclear);
+                response = response.replace('{{{PETROLEUM_COUNT}}}', rows[0].petroleum);
+                response = response.replace('{{{RENEWABLE_COUNT}}}', rows[0].renewable);
+                res.status(200).type('html').send(response); 
+                }
             });
-
-
-            //res.status(200).type('html').send(template); // <-- you may need to change this
         }
     });
 });
@@ -94,3 +100,6 @@ app.get('/energy/:selected_energy_source', (req, res) => {
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
 });
+
+
+
